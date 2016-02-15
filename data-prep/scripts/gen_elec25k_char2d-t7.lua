bb6c753a6fc6500411830535ac7a29f6ef280394
peng sun <pengsun000@gmail.com>
2016-02-14 21:19:10 -0500

    new data gen scripts

--- data-prep/scripts/gen_elec25k_char2d-t7.lua
+++ data-prep/scripts/gen_elec25k_char2d-t7.lua
@@ -2,13 +2,13 @@ require'pl.path'
 local ut = require'util.misc'
 
 --- common opt
-local numClasses = 14
+local numClasses = 2
 
-local dataPath = '/mnt/data/datasets/Text/dbpedia' -- deepml
---local dataPath = '/home/ps/data/dbpedia' -- local
+--local dataPath = '/mnt/data/datasets/Text/elec' -- deepml
+local dataPath = '/home/ps/data/elec' -- local
 
-local dataPathTokCat = path.join(dataPath, 'tok-cat')
-local dataPathOut = path.join(dataPath, 'char2d-t7')
+local dataPathTokCat = dataPath
+local dataPathOut = path.join(dataPath, 'tr25k-char2d-t7')
 
 -- ensure output path
 ut.ensure_path(dataPathOut)
@@ -23,11 +23,11 @@ end
 require'data-prep.tokcat2char2dtensor'.main{
     -- input
     data_path = dataPathTokCat,
-    fn_vocab_freq = 'train-30000.vocab',
-    fn_tok_train = 'train.txt.tok',
-    fn_cat_train = 'train.cat',
-    fn_tok_test = 'test.txt.tok',
-    fn_cat_test = 'test.cat',
+    fn_vocab_freq = 'elec-25k-train-30000.vocab',
+    fn_tok_train = 'elec-25k-train.txt.tok',
+    fn_cat_train = 'elec-25k-train.cat',
+    fn_tok_test = 'elec-test.txt.tok',
+    fn_cat_test = 'elec-test.cat',
     vocab_cat = get_vocab_cat(),
     -- output
     data_out = dataPathOut,
