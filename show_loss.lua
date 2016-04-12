88af6f8fceba2b66ffd1afb285ed7362976147c6
pengsun <pengsun000@gmail.com>
2016-04-11 23:06:15 -0400

    more plots for show_loss.lua

--- show_loss.lua
+++ show_loss.lua
@@ -1,6 +1,17 @@
 require'pl.path'
 require'gnuplot'
 
+--- helpers
+local function get_errVal(fn)
+    require'cudnn'
+    require'OneHot'
+
+    local env = torch.load(fn)
+    local errVal = env.errVal
+
+    collectgarbage()
+    return errVal
+end
 
 local function get_lossesVal(fn)
     require'cudnn'
@@ -30,48 +41,73 @@ local function get_it_val(ell)
     return it, val
 end
 
-local function get_it_val_from_file(fn)
+local function get_itValErr_from_file(fn)
+    local err = get_errVal(fn)
+    local it, val = get_it_val(err)
+    return torch.Tensor(it), torch.Tensor(val)
+end
+
+local function get_itValLoss_from_file(fn)
     local ell = get_lossesVal(fn)
     local it, val = get_it_val(ell)
     return torch.Tensor(it), torch.Tensor(val)
 end
 
-local function get_it_tr_from_file(fn)
+local function get_itTrLoss_from_file(fn)
     local ell = get_lossesTr(fn)
     local it, val = get_it_val(ell)
     return torch.Tensor(it), torch.Tensor(val)
 end
 
----[[]]
+--- configs
 require'onehot-temp-conv'
 local paths = {
-    path.join('cv','imdb-fixtail-word-tmp'),
-    path.join('cv','imdb-fixtail-word-tmp'),
-    path.join('cv','imdb-fixtail-word-tmp'),
+    path.join('cv-sgd', 'yelprevfull-varlen-word-wdOutLay1-bat100-V4'),
+    path.join('cv-sgd', 'yelprevfull-fixtail-word'),
+    path.join('cv-sgd', 'yelprevfull-fixtail-word'),
+    path.join('cv-sgd', 'yelprevfull-fixtail-word'),
 }
 local names = {
-    "M475-HU500-cv2max-o_epoch24.00_lossval0.2757_errval8.81",
-    "M475-HU256-sup-cv2mp2-cv2max-o_epoch18.00_lossval1.9111_errval10.60",
-    "M475-HU500-supV2-cv2mp2-cv2max-o_epoch10.00_lossval1.2181_errval13.06",
+    "HU500-KH3-cv-max-oV4_epoch30.00_lossval0.7877_errval34.32",
+    "M329-HU500-KH3-cv-max-oV4_epoch30.00_lossval0.7997_errval34.79",
+    "M225-HU500-KH3-cv-max-oV4_epoch30.00_lossval0.8089_errval35.05",
+    'M118-HU500-KH3-cv-max-oV4_epoch30.00_lossval0.8516_errval37.12',
+}
+local plot_names = names
+local plot_names = {
+    'var-len',
+    'fix-len-q-90%',
+    'fix-len-q-75%',
+    'fix-len-q-50%',
 }
 
-
-local itemsVal, itemsTr = {}, {}
+--- get stuff
+local itemsErrVal = {}
+local itemsLossVal, itemsLossTr = {}, {}
 for i, name in pairs(names) do
     local fn = path.join(paths[i], name .. ".t7")
+    local plotName = plot_names[i]
 
-    local itVal, val = get_it_val_from_file(fn)
-    table.insert(itemsVal, {name, itVal, val, "~"})
+    local itVal, val = get_itValLoss_from_file(fn)
+    table.insert(itemsLossVal, {plotName, itVal, val, "~"})
 
-    local itTr, tr = get_it_tr_from_file(fn)
-    table.insert(itemsTr, {name, itTr, tr, "~"})
+    local itTr, tr = get_itTrLoss_from_file(fn)
+    table.insert(itemsLossTr, {plotName, itTr, tr, "~"})
+
+    local itVal, val = get_itValErr_from_file(fn)
+    table.insert(itemsErrVal, {plotName, itVal, val, "~"})
 end
 
+--- draw
 gnuplot.figure(1)
 gnuplot.title('val loss')
-gnuplot.plot(table.unpack(itemsVal))
+gnuplot.plot(table.unpack(itemsLossVal))
 
 gnuplot.figure(2)
 gnuplot.title('tr loss')
-gnuplot.plot(table.unpack(itemsTr))
+gnuplot.plot(table.unpack(itemsLossTr))
+
+gnuplot.figure(3)
+gnuplot.title('yelp-full, test err')
+gnuplot.plot(table.unpack(itemsErrVal))
 
