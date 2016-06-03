5b4af3228fe39e8fe3e70b8d5d7c356e0aa289b2
Peng Sun <pengsun000@gamil.com>
2016-06-03 17:52:46 -0400

    update nets

--- net/word-deep/desup.cv-cv-cvmax-o.lua
+++ net/word-deep/desup.cv-cv-cvmax-o.lua
@@ -1,6 +1,7 @@
 require'nn'
 require'cudnn'
 require'ohnn'
+require'nngraph'
 
 --cudnn.fastest = true
 
@@ -15,6 +16,7 @@ this.main = function(opt)
     assert(type(HU)=='table' and #HU == 3) -- 3 layers
 
     local kH = opt.KH or error('no opt.KH')
+
     local function get_pad(width)
         assert(width %2 == 1)
         return (width -1)/2
@@ -23,36 +25,78 @@ this.main = function(opt)
 
     local indUnknown = 1
 
+    local function make_stackedConvLayers()
+
+        local function make_seqConvLayer(nodeX, Hin, Hout)
+            local mconv = ohnn.OneHotTemporalSeqConvolution(Hin, Hout, kH, {
+                hasBias = true, padBegLen = pad, padEndLen = pad, padIndValue = indUnknown
+            })
+
+            -- B, M (,V)
+            local nodeY = mconv (nodeX)
+            -- B, M, H
+            local nodeY2 = nn.Transpose({2,3}) (nodeY)
+            -- B, H, M
+            local nodeY3 = nn.Unsqueeze(3, 2) (nodeY2)
+            -- B, H, M, 1
+            local nodeY4 = cudnn.ReLU(true) (nodeY3)
+            -- B, H, M, 1
+
+            return nodeY4
+        end
+
+        local function make_spatialConvLayer(nodeX, Hin, Hout)
+            local mconv = cudnn.SpatialConvolution(Hin, Hout, 1, kH, 1,1, 0,pad)
+
+            -- B, HIn, M, 1
+            local nodeY = mconv (nodeX)
+            -- B, Hout, M, 1
+            local nodeY2 = cudnn.ReLU(true) (nodeY)
+            -- B, Hout, M, 1
+
+            return nodeY2
+        end
+
+        local x = nn.Identity() ()
+        local x1 = make_seqConvLayer(x, V, HU[1])
+        local x2 = make_spatialConvLayer(x1, HU[1], HU[2])
+        local x3 = make_spatialConvLayer(x2, HU[2], HU[3])
+
+        -- Input: B, M (,V)
+        local m = nn.gModule({x}, {x1, x2, x3})
+        -- Output: {B, HU1, M, 1}, {B, HU2, M, 1}, {B, HU3, M, 1}
+        return m
+    end
+
+    local function make_parOutputLayers()
+        local function make_outputLayer(nHidUnits)
+            local md = nn.Sequential()
+            -- B, H, M, 1
+            md:add( nn.Max(3) )
+            -- B, H, 1
+            md:add( nn.Squeeze(3) )
+            -- B, H
+            md:add( nn.Linear(nHidUnits, K) )
+            -- B, K
+            return md
+        end
+
+        local pt = nn.ParallelTable()
+        -- {B, HU1, M, 1}, {B, HU2, M, 1}, {B, HU3, M, 1}
+        pt:add( make_outputLayer(HU[1]) )
+        pt:add( make_outputLayer(HU[2]) )
+        pt:add( make_outputLayer(HU[3]) )
+        -- {B, K}, {B, K}, {B, K}
+        return pt
+    end
+
     local md = nn.Sequential()
-
     -- B, M (,V)
-    md:add( ohnn.OneHotTemporalSeqConvolution(V, HU[1], kH, {
-        hasBias = true, padBegLen = pad, padEndLen = pad, padIndValue = indUnknown
-    }) )
-    md:add( cudnn.ReLU(true) )
-    -- B, M, HU1
-    md:add( nn.Transpose({2,3}) )
-    -- B, HU1, M
-    md:add( nn.Unsqueeze(3, 2) )
-    -- B, HU1, M, 1
-
-    -- B, HU1, M, 1
-    md:add( cudnn.SpatialConvolution(HU[1], HU[2], 1, kH, 1,1, 0,pad) )
-    md:add( cudnn.ReLU(true) )
-    -- B, HU2, M, 1
-
-    -- B, HU2, M, 1
-    md:add( cudnn.SpatialConvolution(HU[2], HU[3], 1, kH, 1,1, 0,pad) )
-    md:add( cudnn.ReLU(true) )
-    -- B, HU3, M, 1
-    md:add( nn.Max(3) )
-    md:add( nn.Dropout(0.5, false, true) )
-    -- B, HU3, 1
-
-    -- B, HU3, 1
-    md:add( nn.Squeeze(2, 2) )
-    -- B, HU3
-    md:add( nn.Linear(HU[3], K) )
+    md:add( make_stackedConvLayers() )
+    -- {B, HU1, M, 1}, {B, HU2, M, 1}, {B, HU3, M, 1}
+    md:add( make_parOutputLayers() )
+    -- {B, K}, {B, K}, {B, K}
+    md:add( nn.CAddTable(true) )
     -- B, K
     md:add(cudnn.LogSoftMax())
     -- B, K
